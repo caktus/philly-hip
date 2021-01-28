f947e14cd1d9b2a09e4b68eaa31a5324c47262ca
Christopher Dixon <cdixon@caktusgroup.com>
2021-01-28 16:13:27 -0500

    Add scss stylesheets to base.html template using django-scss-processor. Import bulma.io stylesheet into scss init.scss

--- apps/hip/views.py
+++ apps/hip/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 
 def handler404(request, *args, **argv):
@@ -11,3 +12,7 @@ def handler500(request, *args, **argv):
     response = render(request, "500.html", {})
     response.status_code = 500
     return response
+
+
+def home(request):
+    return TemplateResponse(request, "home.html")
