84575e2758f300336153f967d35337092b2adc6f
Vinod Kurup <vinod@kurup.com>
2021-02-08 12:43:29 -0500

    Use SCSS for wagtail_styles

--- apps/hip/wagtail_hooks.py
+++ apps/hip/wagtail_hooks.py
@@ -1,6 +1,6 @@
-from django.templatetags.static import static
 from django.utils.html import format_html
 
+from sass_processor.processor import sass_processor
 from wagtail.core import hooks
 
 
@@ -8,5 +8,6 @@ from wagtail.core import hooks
 def editor_css():
     """Register the extra CSS styles to be used for the Wagtail editor."""
     return format_html(
-        '<link rel="stylesheet" href="{}">', static("styles/wagtail_styles.css")
+        '<link rel="stylesheet" href="{}">',
+        sass_processor("styles/wagtail_styles.scss"),
     )
