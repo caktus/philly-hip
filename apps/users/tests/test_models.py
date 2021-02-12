dbde61e0951b0eee12e33eb97393ffa7d1939a07
Vinod Kurup <vinod@kurup.com>
2021-02-12 10:59:52 -0500

    Use pytest fixtures

--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -2,46 +2,50 @@ import pytest
 from apps.users.tests.factories import DEFAULT_PASSWORD, UserFactory
 
 
-pytestmark = pytest.mark.django_db
+@pytest.fixture
+def user(db):
+    return UserFactory()
 
 
-def test_user_get_full_name():
-    user = UserFactory()
+@pytest.fixture
+def superuser(db):
+    return UserFactory(is_superuser=True)
+
+
+def test_user_get_full_name(user):
     assert user.get_full_name() == f"{user.first_name} {user.last_name}"
 
 
-def test_user_get_full_name_if_first_name_empty():
-    user = UserFactory(first_name="")
+def test_user_get_full_name_if_first_name_empty(user):
+    user.first_name = ""
     assert user.get_full_name() == user.last_name
 
 
-def test_user_get_full_name_if_last_name_empty():
-    user = UserFactory(first_name="", last_name="")
+def test_user_get_full_name_if_last_name_empty(user):
+    user.last_name = ""
     assert user.get_full_name() == user.first_name
 
 
-def test_user_get_full_name_if_name_fields_empty():
-    user = UserFactory(first_name="", last_name="")
+def test_user_get_full_name_if_name_fields_empty(user):
+    user.first_name = user.last_name = ""
     assert user.get_full_name() == ""
 
 
-def test_user_get_short_name():
-    user = UserFactory()
+def test_user_get_short_name(user):
     assert user.get_short_name() == user.first_name
 
 
-def test_str_method():
-    user = UserFactory()
+def test_str_method(user):
     assert str(user) == user.email
 
 
-def test_superuser():
-    user = UserFactory(is_superuser=True)
-    assert user.is_superuser
+def test_superuser(superuser):
+    assert superuser.is_superuser
 
 
-def test_case_insensitive_login(client):
+def test_case_insensitive_login(client, user):
     mixed_case_email = "Mixed.Case@example.com"
-    user = UserFactory(email=mixed_case_email)
+    user.email = mixed_case_email
+    user.save()
     # lowercase the email and they should be able to login
     assert client.login(email=mixed_case_email.lower(), password=DEFAULT_PASSWORD)
