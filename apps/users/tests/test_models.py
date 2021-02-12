b1be86c1f2b4d9d6c67e031f3b2b78614d5070f3
Vinod Kurup <vinod@kurup.com>
2021-02-12 11:11:25 -0500

    Use pytest-factoryboy

--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -1,49 +1,43 @@
-import pytest
 from apps.users.tests.factories import DEFAULT_PASSWORD, UserFactory
+from pytest_factoryboy import register
 
 
-@pytest.fixture
-def user(db):
-    return UserFactory()
+register(UserFactory)
 
 
-@pytest.fixture
-def superuser(db):
-    return UserFactory(is_superuser=True)
-
-
-def test_user_get_full_name(user):
+def test_user_get_full_name(db, user):
     assert user.get_full_name() == f"{user.first_name} {user.last_name}"
 
 
-def test_user_get_full_name_if_first_name_empty(user):
+def test_user_get_full_name_if_first_name_empty(db, user):
     user.first_name = ""
     assert user.get_full_name() == user.last_name
 
 
-def test_user_get_full_name_if_last_name_empty(user):
+def test_user_get_full_name_if_last_name_empty(db, user):
     user.last_name = ""
     assert user.get_full_name() == user.first_name
 
 
-def test_user_get_full_name_if_name_fields_empty(user):
+def test_user_get_full_name_if_name_fields_empty(db, user):
     user.first_name = user.last_name = ""
     assert user.get_full_name() == ""
 
 
-def test_user_get_short_name(user):
+def test_user_get_short_name(db, user):
     assert user.get_short_name() == user.first_name
 
 
-def test_str_method(user):
+def test_str_method(db, user):
     assert str(user) == user.email
 
 
-def test_superuser(superuser):
+def test_superuser(db, user_factory):
+    superuser = user_factory(is_superuser=True)
     assert superuser.is_superuser
 
 
-def test_case_insensitive_login(client, user):
+def test_case_insensitive_login(client, db, user):
     mixed_case_email = "Mixed.Case@example.com"
     user.email = mixed_case_email
     user.save()
