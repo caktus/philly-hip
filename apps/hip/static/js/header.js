38211d5dceb39f034b1e68db402525d1b852ead1
Christopher Dixon <cdixon@caktusgroup.com>
2021-02-17 11:02:02 -0500

    No results, search modal

--- apps/hip/static/js/header.js
+++ apps/hip/static/js/header.js
@@ -1,12 +1,27 @@
 export default function() {
-  const navbarBurger = document.querySelector("#navbar-burger");
-  navbarBurger.addEventListener("click", function () {
-    const dataTarget = this.dataset.target;
-    const target = document.getElementById(dataTarget);
-    if (target.classList.contains("is-hidden-touch")) {
-      target.classList.remove("is-hidden-touch");
-    } else {
-      target.classList.add("is-hidden-touch");
-    }
-  });
+  navbarBurger();
+  showHideMobileSearchModal();
+
+  function navbarBurger() {
+    // This button handles the show/hide functionality of sidebar menu
+    const navbarBurger = document.querySelector("#navbar-burger");
+    navbarBurger.addEventListener("click", function () {
+      const dataTarget = this.dataset.target;
+      const target = document.getElementById(dataTarget);
+      if (target.classList.contains("is-hidden-touch")) {
+        target.classList.remove("is-hidden-touch");
+      } else {
+        target.classList.add("is-hidden-touch");
+      }
+    });
+  }
+
+  function showHideMobileSearchModal() {
+    /* Handles the show/hide functionality of the mobile search container
+
+      - search id="search-mobile-btn" in header.html to view the button
+      - view mobile_search_modal.html for the content that is being shown/hidden
+    */
+    
+  }
 };
