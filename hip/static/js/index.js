3ee0f6947ef31f95da60a2375ed1abc22058f3e1
Christopher Dixon <cdixon@caktusgroup.com>
2021-02-19 09:41:03 -0500

    style document links

--- hip/static/js/index.js
+++ hip/static/js/index.js
@@ -18,12 +18,14 @@ import JQuery from "jquery";
   *
  */
 
+import Common from "./common";
 import Header from "../../../apps/hip/static/js/header";
 import SideBar from "../../../apps/hip/static/js/sidebar";
 import RightScrollNav from "../../../apps/hip/static/js/rightScrollNav";
 import MobileSearchModal from "../../../apps/hip/static/js/mobileSearchModal";
 
 document.addEventListener("DOMContentLoaded", function() {
+  Common();
   Header();
   SideBar();
   MobileSearchModal();
