16d0e36ecfa421648add6b23092cda29f21e027b
Dmitriy Chukhin <dchukhin@gmail.com>
2021-02-22 12:21:44 -0500

    create a context processor to return homepage URL for each request

--- apps/common/context_processors.py
+++ apps/common/context_processors.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from apps.hip.models import HomePage
+
 
 def sentry_dsn(request):
     return {"SENTRY_DSN": settings.SENTRY_DSN}
@@ -7,3 +9,9 @@ def sentry_dsn(request):
 
 def commit_sha(request):
     return {"COMMIT_SHA": settings.COMMIT_SHA}
+
+
+def home_page_url(request):
+    """Return the URL for the home page."""
+    home_page = HomePage.objects.live().first()
+    return {"home_page_url": home_page.url if home_page else "/"}
