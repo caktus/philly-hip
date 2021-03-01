a1029a7486170d54ec76d4aa43f923354b872054
Dmitriy Chukhin <dchukhin@gmail.com>
2021-03-01 07:39:02 -0500

    make tests for DiseasePage.emergent_date_range() more readable

--- apps/disease_control/tests/test_models.py
+++ apps/disease_control/tests/test_models.py
@@ -1,4 +1,4 @@
-from django.utils.timezone import now, timedelta
+from datetime import date
 
 from .factories import DiseasePageFactory
 
@@ -22,73 +22,58 @@ def test_disease_page_emergent_date_range_no_dates(db):
 
 def test_disease_page_emergent_date_range_only_begin_date(db):
     """Test the emergent_date_range for a DiseasePage with only an emergent_begin_date."""
-    date_yesterday = now().date() - timedelta(days=1)
+    date_2020_01_01 = date(year=2020, month=1, day=1)
 
     disease_page_emergent = DiseasePageFactory(
         is_emergent=True,
-        emergent_begin_date=date_yesterday,
+        emergent_begin_date=date_2020_01_01,
         emergent_end_date=None,
     )
     disease_page_nonemergent = DiseasePageFactory(
         is_emergent=False,
-        emergent_begin_date=date_yesterday,
+        emergent_begin_date=date_2020_01_01,
         emergent_end_date=None,
     )
 
-    assert (
-        f"{date_yesterday.strftime('%b %-d, %Y')} - Present"
-        == disease_page_emergent.emergent_date_range
-    )
-    assert (
-        f"{date_yesterday.strftime('%b %-d, %Y')} - Present"
-        == disease_page_nonemergent.emergent_date_range
-    )
+    assert "Jan 1, 2020 - Present" == disease_page_emergent.emergent_date_range
+    assert "Jan 1, 2020 - Present" == disease_page_nonemergent.emergent_date_range
 
 
 def test_disease_page_emergent_date_range_only_end_date(db):
     """Test the emergent_date_range for a DiseasePage with only an emergent_end_date."""
-    date_yesterday = now().date() - timedelta(days=1)
+    date_2020_01_01 = date(year=2020, month=1, day=1)
 
     disease_page_emergent = DiseasePageFactory(
         is_emergent=True,
         emergent_begin_date=None,
-        emergent_end_date=date_yesterday,
+        emergent_end_date=date_2020_01_01,
     )
     disease_page_nonemergent = DiseasePageFactory(
         is_emergent=False,
         emergent_begin_date=None,
-        emergent_end_date=date_yesterday,
+        emergent_end_date=date_2020_01_01,
     )
 
-    assert (
-        f"Until {date_yesterday.strftime('%b %-d, %Y')}"
-        == disease_page_emergent.emergent_date_range
-    )
-    assert (
-        f"Until {date_yesterday.strftime('%b %-d, %Y')}"
-        == disease_page_nonemergent.emergent_date_range
-    )
+    assert "Until Jan 1, 2020" == disease_page_emergent.emergent_date_range
+    assert "Until Jan 1, 2020" == disease_page_nonemergent.emergent_date_range
 
 
 def test_disease_page_emergent_date_range_start_and_end_date(db):
     """Test emergent_date_range for DiseasePage with emergent_begin_date and emergent_end_date."""
-    date_yesterday = now().date() - timedelta(days=1)
-    date_500_days_ago = now().date() - timedelta(days=500)
+    date_2019_12_12 = date(year=2019, month=12, day=12)
+    date_2021_09_09 = date(year=2021, month=9, day=9)
 
     disease_page_emergent = DiseasePageFactory(
         is_emergent=True,
-        emergent_begin_date=date_500_days_ago,
-        emergent_end_date=date_yesterday,
+        emergent_begin_date=date_2019_12_12,
+        emergent_end_date=date_2021_09_09,
     )
     disease_page_nonemergent = DiseasePageFactory(
         is_emergent=False,
-        emergent_begin_date=date_500_days_ago,
-        emergent_end_date=date_yesterday,
+        emergent_begin_date=date_2019_12_12,
+        emergent_end_date=date_2021_09_09,
     )
 
-    expected_str = (
-        f"{date_500_days_ago.strftime('%b %-d, %Y')} - "
-        f"{date_yesterday.strftime('%b %-d, %Y')}"
-    )
+    expected_str = "Dec 12, 2019 - Sep 9, 2021"
     assert expected_str == disease_page_emergent.emergent_date_range
     assert expected_str == disease_page_nonemergent.emergent_date_range
