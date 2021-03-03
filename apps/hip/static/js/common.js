f12a19a20f459a8f43709b7b26e7934abf09f110
Vinod Kurup <vinod@kurup.com>
2021-03-03 12:39:41 -0500

    unstick and restick the navbar when modals open/close

--- apps/hip/static/js/common.js
+++ apps/hip/static/js/common.js
@@ -2,10 +2,16 @@ export function openMobileModal (target) {
   // target: element to open as a modal
   target.classList.remove("is-hidden-touch");
   document.body.classList.add('modal-is-open-hip')
+  // un-stick the navbar
+  const navbar = document.getElementById("mobile-navbar-hip");
+  navbar.classList.remove("is-sticky-hip");
 };
 
 export function closeMobileModal (target) {
-  // target: element to open as a modal
+  // target: element representing the modal to close
   target.classList.add("is-hidden-touch");
   document.body.classList.remove('modal-is-open-hip')
+  // re-stick the navbar
+  const navbar = document.getElementById("mobile-navbar-hip");
+  navbar.classList.add("is-sticky-hip");
 };
