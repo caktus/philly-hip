3579b3084963e305375e48c24cda6a205b417d7b
Dmitriy Chukhin <dchukhin@gmail.com>
2021-03-05 20:34:36 -0500

    add an EmergentHealthTopicsPage

--- apps/disease_control/tests/factories.py
+++ apps/disease_control/tests/factories.py
@@ -1,7 +1,7 @@
 import factory
 import wagtail_factories
 
-from ..models import DiseasePage
+from ..models import DiseasePage, EmergentHealthTopicsPage
 
 
 class DiseasePageFactory(wagtail_factories.PageFactory):
@@ -9,3 +9,10 @@ class DiseasePageFactory(wagtail_factories.PageFactory):
 
     class Meta:
         model = DiseasePage
+
+
+class EmergentHealthTopicsPageFactory(wagtail_factories.PageFactory):
+    title = factory.faker.Faker("sentence")
+
+    class Meta:
+        model = EmergentHealthTopicsPage
