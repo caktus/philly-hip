f6ef96bed8c88e91e6761bd6be5a12035c94dc77
Dmitriy Chukhin <dchukhin@gmail.com>
2021-03-10 13:43:29 -0500

    style external links to have domain name just after external link icon

--- hip/static/js/common.js
+++ hip/static/js/common.js
@@ -1,6 +1,6 @@
 export default function () {
   manageDocumentLinks();
-  addExternalLinkIcons();
+  addExternalLinkIconsAndDomain();
 
   function manageDocumentLinks () {
     /* Manage Document Links
@@ -11,7 +11,7 @@ export default function () {
     This function finds links to documents. If found,
     the link color is changed to $dark-blue, and the associated icon
     is displayed if found as a sibling of the link's parent container.
-    
+
     <td> <!-- some container tag -->
       <p><a href="/documents/path-to/document"</a></p>
       <span class="external-link-icon-hip pt-3 pl-4 is-hidden"></span>
@@ -29,16 +29,47 @@ export default function () {
     });
   }
 
-  function addExternalLinkIcons () {
+  function addExternalLinkIconsAndDomain () {
     /* Add External Link Icons to Wagtail Hook Generated Links of
     type external across the site.
+     * Also, some external links get the link's domain inside of parentheses
+     * added just after the icon. For example: for a link with an href to
+     * cdc.gov/somethinig, we might add (cdc.gov) just after the icon.
+     * We determine if the domain name needs to be added by seeing if the external
+     * link is a child of an HTML element with a specific CSS class.
     */
    const externalLinks = Array.from(document.querySelectorAll('a.external-linktype'));
+
+   // External links that are children of an HTML element with the following CSS
+   // class will also get a domain name just after the icon.
+   const parentsThatGetDomainForExternalLink = Array.from(document.querySelectorAll('.external-links-with-domains-hip'));
+
    for (const link of externalLinks) {
+     // Add the external link icon.
      const span = document.createElement("span");
      span.classList.add("external-link-icon-hip");
      span.classList.add("pl-2");
      link.appendChild(span);
+
+     // Is this link contained within one of the parentsThatGetDomainForExternalLink?
+     let needToAddDomain = false;
+     for (const parent of parentsThatGetDomainForExternalLink) {
+       if (parent.contains(link) === true) {
+         needToAddDomain = true;
+         break;
+       }
+     }
+     // If the link is a child of one of the parentsThatGetDomainForExternalLinks,
+     // then add the domain name just after the external link icon.
+     if (needToAddDomain === true) {
+       const domainName = link.href.split("://").pop().split("/")[0];
+
+       const domainSpan = document.createElement("span");
+       domainSpan.innerHTML = `(${domainName})`;
+       domainSpan.classList.add("external-link-domain-hip");
+       domainSpan.classList.add("pl-2");
+       link.appendChild(domainSpan);
+     }
    };
   };
 };
