d918472e6b244c5831ead2b3cf4caf805ecdc89b
Dmitriy Chukhin <dchukhin@gmail.com>
2021-03-13 08:16:37 -0500

    rename incorrectly named factory to its correct name

--- apps/emergency_response/tests/factories.py
+++ apps/emergency_response/tests/factories.py
@@ -4,7 +4,7 @@ import wagtail_factories
 from ..models import EmergencyResponsePage
 
 
-class EmergentHealthTopicsPageFactory(wagtail_factories.PageFactory):
+class EmergencyResponsePageFactory(wagtail_factories.PageFactory):
     title = factory.faker.Faker("sentence")
     description = factory.faker.Faker("text")
 
