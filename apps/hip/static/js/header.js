b1037e0a9835b16547a9b3b5a71182a33faf5b5f
Vinod Kurup <vinod@kurup.com>
2021-03-16 12:29:57 -0400

    Get mobile views working

--- apps/hip/static/js/header.js
+++ apps/hip/static/js/header.js
@@ -2,7 +2,8 @@ import { openMobileModal, closeMobileModal } from "./common";
 
 export default function() {
   navbarBurger();
-  showHideMobileSearchModal();
+  setUpMobileSearchModal();
+  showMobileSearchModal();
 
   function navbarBurger() {
     // This button handles the show/hide functionality of sidebar menu
@@ -18,14 +19,14 @@ export default function() {
     });
   }
 
-  function showHideMobileSearchModal() {
+  function setUpMobileSearchModal() {
     /* Handles the show/hide functionality of the mobile search container
 
       - search id="search-mobile-btn" in header.html to view the button
       - view mobile_search_modal.html for the content that is being shown/hidden
     */
     const searchMobileBtn = document.querySelector("#search-mobile-btn");
-    
+
     searchMobileBtn.addEventListener("click", function () {
       const dataTarget = this.dataset.target;
       const target = document.getElementById(dataTarget);
@@ -35,5 +36,17 @@ export default function() {
         closeMobileModal(target);
       }
     });
+  }
+
+  function showMobileSearchModal() {
+    // If we are on the search page, and on mobile/tablet widths, then we should automatically show the modal
+    const isSearchPage = document.querySelector(".search-page-hip");
+    const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+    if (isSearchPage && width < 1024) {
+      const searchModal = document.querySelector("#searchMobileContainer");
+      openMobileModal(searchModal);
+    }
+
   }
 };
