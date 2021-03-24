f9157c15ca4e88e5cb33b4091cc30d43530e9839
Christopher Dixon <cdixon@caktusgroup.com>
2021-03-24 10:47:03 -0400

    remove sticky-header when health alerts signup form is open.

--- apps/health_alerts/static/js/healthAlertSubscriber.js
+++ apps/health_alerts/static/js/healthAlertSubscriber.js
@@ -1,8 +1,12 @@
 export default function() {
   const healthAlertSubscriberEl = document.querySelector(".health-alert-subscriber-modal");
+  const mobileHeader = document.getElementById("mobile-header-hip");
   
   if (healthAlertSubscriberEl) {
     document.body.classList.add("modal-is-open-hip");
+    if (mobileHeader) {
+      mobileHeader.classList.remove("is-sticky-hip");
+    }
     styleInputElements();
     closeBtnClicked();
   }
@@ -24,6 +28,9 @@ export default function() {
     const closeBtn = document.querySelector("#health-alert-subscriber-close-btn-hip");
     closeBtn.addEventListener("click", function () {
       document.body.classList.remove("modal-is-open-hip");
+      if (mobileHeader) {
+        mobileHeader.classList.add("is-sticky-hip");
+      }
       window.location.href = this.dataset.href;
     });
   }
