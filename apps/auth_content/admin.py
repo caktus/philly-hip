d9fe135175c82b6f0783c3d9aedb3c577f2b7891
Christopher Dixon <cdixon@caktusgroup.com>
2021-05-16 15:51:47 -0400

    Make ClosedPODContactInformation available for export. Create a blacklist resource for omitting fields from the export of the User model (passwords have been excluded)

---
+++ apps/auth_content/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from import_export.admin import ExportMixin
+
+from .models import ClosedPODContactInformation
+
+
+@admin.register(ClosedPODContactInformation)
+class ClosedPODContactInformationAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "facility_name",
+        "facility_id",
+        "primary_contact_name",
+        "secondary_contact_name",
+    )
+    list_filter = (
+        "user",
+        "facility_name",
+        "facility_id",
+        "primary_contact_name",
+        "secondary_contact_name",
+    )
+    search_fields = (
+        "user",
+        "facility_name",
+        "facility_id",
+        "primary_contact_name",
+        "secondary_contact_name",
+    )
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,11 +3,24 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
 from import_export.admin import ExportMixin
+from import_export.resources import ModelResource
 
 from .models import User
 
 
+class UserResourceBlackList(ModelResource):
+    """Provides blacklist for fields that should not be exported.
+    Currently this resource omits the password field from being exported.
+    """
+
+    class Meta:
+        model = User
+        exclude = ("password",)
+
+
 class CustomUserAdmin(ExportMixin, UserAdmin):
+    resource_class = UserResourceBlackList
+
     list_display = ("id", "email", "created", "modified")
     list_filter = ("is_active", "is_staff", "groups")
     search_fields = ("email",)
