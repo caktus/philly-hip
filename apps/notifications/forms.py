c44096c6ac9fe4ae4f723e8954fe9cbceacb308b
Dmitriy Chukhin <dchukhin@gmail.com>
2021-06-11 08:50:28 -0400

    use InternalAlertsSubscriberForm for InternalAlertsSubscriber objects

--- apps/notifications/forms.py
+++ apps/notifications/forms.py
@@ -1,10 +1,49 @@
-from django.forms import Form
+from django.forms import Form, ModelForm
+
+from .models import InternalEmployeeAlertSubscriber
 
 
-# TODO for DIS-1700: make this form a ModelForm, similar to the HealthAlertSubscriberForm.
-class InternalAlertsSubscriberForm(Form):
-    def save(self, *args, **kwargs):
-        pass
+class InternalAlertsSubscriberForm(ModelForm):
+    use_required_attribute = False
+
+    class Meta:
+        model = InternalEmployeeAlertSubscriber
+        fields = "__all__"
+
+    def about_you_fields(self):
+        about_you_field_names = [
+            "first_name",
+            "last_name",
+            "professional_license",
+            "languages_spoken",
+            "division",
+        ]
+        return [self[name] for name in self.fields if name in about_you_field_names]
+
+    def contact_information_fields(self):
+        # return [self[name] for name in self.fields if name.startswith("agency_")]
+        contact_info_field_names = [
+            "work_phone",
+            "work_email",
+            "cell_phone",
+            "personal_email",
+            "home_phone",
+            "street_address",
+            "city",
+            "state",
+            "zip_code",
+        ]
+        return [self[name] for name in self.fields if name in contact_info_field_names]
+
+    def form_sections(self):
+        """Return the sections of this form, including a header, and the fields in the section."""
+        return [
+            {"header": "About You", "fields": self.about_you_fields()},
+            {
+                "header": "Contact Information",
+                "fields": self.contact_information_fields(),
+            },
+        ]
 
 
 # TODO for DIS-1700: make this form a ModelForm, similar to the HealthAlertSubscriberForm.
