31a0a76588ee0a92fa2c05b6b87484d54d1d5e83
Dmitriy Chukhin <dchukhin@gmail.com>
2021-06-15 13:42:16 -0400

    use CommunityResponseSubscriberForm for CommunityResponseSubscriber objects

--- apps/notifications/forms.py
+++ apps/notifications/forms.py
@@ -1,6 +1,6 @@
 from django.forms import Form, ModelForm
 
-from .models import InternalEmployeeAlertSubscriber
+from .models import CommunityResponseSubscriber, InternalEmployeeAlertSubscriber
 
 
 class InternalAlertsSubscriberForm(ModelForm):
@@ -46,10 +46,54 @@ class InternalAlertsSubscriberForm(ModelForm):
         ]
 
 
-# TODO for DIS-1700: make this form a ModelForm, similar to the HealthAlertSubscriberForm.
-class CommunityResponseSubscriberForm(Form):
-    def save(self, *args, **kwargs):
-        pass
+class CommunityResponseSubscriberForm(ModelForm):
+    use_required_attribute = False
+
+    class Meta:
+        model = CommunityResponseSubscriber
+        fields = "__all__"
+
+    def about_you_fields(self):
+        about_you_field_names = [
+            "first_name",
+            "last_name",
+            "organization_name",
+            "title",
+        ]
+        return [self[name] for name in self.fields if name in about_you_field_names]
+
+    def contact_information_fields(self):
+        contact_info_field_names = [
+            "email_address",
+            "cell_phone",
+        ]
+        return [self[name] for name in self.fields if name in contact_info_field_names]
+
+    def organization_info_fields(self):
+        organization_info_field_names = [
+            "organization_street_address",
+            "organization_po_box",
+            "organization_zip_code",
+            "organization_zip_codes_served",
+            "organization_community_members_served",
+        ]
+        return [
+            self[name] for name in self.fields if name in organization_info_field_names
+        ]
+
+    def form_sections(self):
+        """Return the sections of this form, including a header, and the fields in the section."""
+        return [
+            {"header": "About You", "fields": self.about_you_fields()},
+            {
+                "header": "Contact Information",
+                "fields": self.contact_information_fields(),
+            },
+            {
+                "header": "Organization Information",
+                "fields": self.organization_info_fields(),
+            },
+        ]
 
 
 # TODO for DIS-1700: make this form a ModelForm, similar to the HealthAlertSubscriberForm.
