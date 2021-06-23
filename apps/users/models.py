4dc236525c52b59e9a0f8bdd3851bbb667a893db
Dmitriy Chukhin <dchukhin@gmail.com>
2021-06-23 08:23:06 -0400

    replace deprecated ugettext_lazy with gettext_lazy

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.postgres.fields import CIEmailField
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import IndexedTimeStampedModel
 from apps.users.managers import UserManager
