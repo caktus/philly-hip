45273b7aa2595ef8330ea7fbdf2750eb3f4f93d9
ronardcaktus <rluna@caktusgroup.com>
2025-04-29 18:34:08 +0000

    Update Wagtail to latest and add missing app

--- hip/settings/base.py
+++ hip/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += [
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
+    "wagtail.contrib.search_promotions",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
