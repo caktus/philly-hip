f199b115e157f7b14fbd6dd92b0a8f5fa1900048
Ronard <87770895+ronardcaktus@users.noreply.github.com>
2026-02-05 13:19:00 -0500

    Replace sign up link (#318)

--- apps/health_alerts/views.py
+++ apps/health_alerts/views.py
@@ -3,6 +3,9 @@ from apps.notifications.views import generic_notification_signup
 from .forms import HealthAlertSubscriberForm
 
 
+# On 02/04/26 Brandon Horvath emailed us asking if we could link
+# to a new form and stop using this one. He said that this form
+# can be deleted in the future.
 def subscribe(request):
     """View for managing sign ups for health alerts."""
     success_message = (
